6fd48203ba383c5066f3fe98d96e40952c211abc
Veyis Turgut <vturgut68@gmail.com>
2020-03-12 20:28:12 +0300

    Update Hyp86.java

--- 230Proje/src/Hyp86.java
+++ 230Proje/src/Hyp86.java
@@ -2,8 +2,12 @@ import java.io.File;
 
 public class Hyp86 {
 
-	char[] memory = new char[64 * 1024];
+	byte[] memory = new byte[64 * 1024];
 	// hexa tutalým
+	char[] di = new char[4];
+	char[] sp = new char[4];
+	char[] si = new char[4];
+	char[] bp = new char[4];
 	char[] ax = new char[4];
 	char[] bx = new char[4];
 	char[] cx = new char[4];
@@ -12,10 +16,13 @@ public class Hyp86 {
 	boolean AF = false;
 	boolean CF = false;
 	String SF = "FFFE";
+	int MP = 0; // memory Pointer
 	boolean OF = false;
-	Hyp86(String s){
-		
+
+	Hyp86(String s) { // constructor
+
 	}
+
 	public static void add(String first, String second) {
 
 	}
@@ -27,121 +34,176 @@ public class Hyp86 {
 	// first reg veya memory
 	// second boyutu firstten büyükse hata ver
 
+	public void mov_mem_xx(String first, String second) {
+
+	}
+
+	public void mov_ax_unknown(String second) {
+		if (second.contains("[") && second.contains("]")) { // second is memory
+
+		} else if (second.equals("bx")) {
+			for (int i = 3; i >= 0; i--) {
+				ax[i] = bx[i];
+			}
+		} else if (second.equals("cx")) {
+			for (int i = 3; i >= 0; i--) {
+				ax[i] = cx[i];
+			}
+		} else if (second.equals("dx")) {
+			for (int i = 3; i >= 0; i--) {
+				ax[i] = dx[i - 4];
+			}
+		} else if (second.contains("l") || second.contains("h")) {// error
+			System.out.println("Error: Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else {// number hexa olabilir (sonunda h ile veya baþýnda 0), decimal olabilir
+
+		}
+	}
+
+	public void mov_bx_unknown(String second) {
+		if (second.contains("[") && second.contains("]")) { // second is memory
+
+		} else if (second.contains("ax")) {
+			for (int i = 3; i >= 0; i--) {
+				bx[i] = ax[i];
+			}
+		} else if (second.contains("cx")) {
+			for (int i = 3; i >= 0; i--) {
+				bx[i] = cx[i];
+			}
+		} else if (second.contains("dx")) {
+			for (int i = 3; i >= 0; i--) {
+				bx[i] = dx[i];
+			}
+		} else if (second.contains("l") || second.contains("h")) {// error
+			System.out.println("Error: Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else {// number
+
+		}
+
+	}
+
+	public void mov_cx_unknown(String second) {
+		if (second.contains("[") && second.contains("]")) { // second is memory
+
+		} else if (second.contains("ax")) {
+			for (int i = 3; i >= 0; i--) {
+				cx[i] = ax[i];
+			}
+		} else if (second.contains("bx")) {
+			for (int i = 3; i >= 0; i--) {
+				cx[i] = bx[i];
+			}
+		} else if (second.contains("dx")) {
+			for (int i = 3; i >= 0; i--) {
+				cx[i] = dx[i];
+			}
+		} else if (second.contains("l") || second.contains("h")) {// error
+			System.out.println("Error: Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else {// number
+
+		}
+
+	}
+
+	public void mov_dx_unknown(String second) {
+		if (second.contains("[") && second.contains("]")) { // second is memory
+
+		} else if (second.contains("ax")) {
+			for (int i = 3; i >= 0; i--) {
+				dx[i] = ax[i];
+			}
+		} else if (second.contains("bx")) {
+			for (int i = 3; i >= 0; i--) {
+				dx[i] = bx[i];
+			}
+		} else if (second.contains("cx")) {
+			for (int i = 3; i >= 0; i--) {
+				dx[i] = cx[i];
+			}
+		} else if (second.contains("l") || second.contains("h")) {// error
+			System.out.println("Error: Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else {// number
+		}
+	}
+
+	public void mov_al_unknown(String second) {
+
+	}
+
+	public void mov_ah_unknown(String second) {
+
+	}
+
+	public void mov_bl_unknown(String second) {
+
+	}
+
+	public void mov_bh_unknown(String second) {
+
+	}
+
+	public void mov_cl_unknown(String second) {
+
+	}
+
+	public void mov_ch_unknown(String second) {
+
+	}
+
+	public void mov_dl_unknown(String second) {
+
+	}
+
+	public void mov_dh_unknown(String second) {
+
+	}
+
+	public void mov_reg_unknown(String first, String second) {
+
+		if (first.equalsIgnoreCase("ax")) { // mov *x,*l ya da *x, *h hatasý yapýldý , mov *x,*x yapýldý,
+			mov_ax_unknown(second);
+		} else if (first.equalsIgnoreCase("bx")) {
+			mov_bx_unknown(second);
+		} else if (first.equalsIgnoreCase("cx")) {
+			mov_cx_unknown(second);
+		} else if (first.equalsIgnoreCase("dx")) {
+			mov_dx_unknown(second);
+		} else if (first.equalsIgnoreCase("al")) {
+			mov_al_unknown(second);
+		} else if (first.equalsIgnoreCase("ah")) {
+			mov_ah_unknown(second);
+		} else if (first.equalsIgnoreCase("bl")) {
+			mov_bl_unknown(second);
+		} else if (first.equalsIgnoreCase("bh")) {
+			mov_bh_unknown(second);
+		} else if (first.equalsIgnoreCase("cl")) {
+			mov_cl_unknown(second);
+		} else if (first.equalsIgnoreCase("ch")) {
+			mov_ch_unknown(second);
+		} else if (first.equalsIgnoreCase("dl")) {
+			mov_dl_unknown(second);
+		} else if (first.equalsIgnoreCase("dh")) {
+			mov_dh_unknown(second);
+		}
+	}
+
 	public void mov(String first, String second) {
 
 		if (first.contains("[") && first.contains("]")) { // first is memory
+
+			mov_mem_xx(first, second);
 
 		} else if (first.equals("ax") || first.equals("cx") || first.equals("bx") || first.equals("dx")
 				|| first.equals("ah") || first.equals("al") || first.equals("bl") || first.equals("bh")
 				|| first.equals("ch") || first.equals("cl") || first.equals("dl") || first.equals("dh")) { // first is
 																											// reg
+			mov_reg_unknown(first, second);
 
-			if (first.equalsIgnoreCase("ax")) { // mov ax,al hatasý +, mov bx,cx +,
-				if (second.contains("[") && second.contains("]")) { // second is memory
-
-				} else if (second.equals("bx")) {
-					for (int i = 3; i >= 0; i--) {
-						ax[i] = bx[i];
-					}
-				} else if (second.equals("cx")) {
-					for (int i = 3; i >= 0; i--) {
-						ax[i] = cx[i];
-					}
-				} else if (second.equals("dx")) {
-					for (int i = 3; i >= 0; i--) {
-						ax[i] = dx[i - 4];
-					}
-				} else if (second.contains("l") || second.contains("h")) {// error
-					System.out.println("Error: Byte/Word Combination Not Allowed");
-					System.exit(0);
-				} else {// number hexa olabilir (sonunda h ile veya baþýnda 0), decimal olabilir
-
-				}
-			}
-
-			// bx
-			else if (first.equalsIgnoreCase("bx")) {
-				if (second.contains("[") && second.contains("]")) { // second is memory
-
-				} else if (second.contains("ax")) {
-					for (int i = 3; i >= 0; i--) {
-						bx[i] = ax[i];
-					}
-				} else if (second.contains("cx")) {
-					for (int i = 3; i >= 0; i--) {
-						bx[i] = cx[i];
-					}
-				} else if (second.contains("dx")) {
-					for (int i = 3; i >= 0; i--) {
-						bx[i] = dx[i];
-					}
-				} else if (second.contains("l") || second.contains("h")) {// error
-					System.out.println("Error: Byte/Word Combination Not Allowed");
-					System.exit(0);
-				} else {// number
-
-				}
-			}
-
-			else if (first.equalsIgnoreCase("cx")) {
-				if (second.contains("[") && second.contains("]")) { // second is memory
-
-				} else if (second.contains("ax")) {
-					for (int i = 3; i >= 0; i--) {
-						cx[i] = ax[i];
-					}
-				} else if (second.contains("bx")) {
-					for (int i = 3; i >= 0; i--) {
-						cx[i] = bx[i];
-					}
-				} else if (second.contains("dx")) {
-					for (int i = 3; i >= 0; i--) {
-						cx[i] = dx[i];
-					}
-				} else if (second.contains("l") || second.contains("h")) {// error
-					System.out.println("Error: Byte/Word Combination Not Allowed");
-					System.exit(0);
-				} else {// number
-
-				}
-			} else if (first.equalsIgnoreCase("dx")) {
-				if (second.contains("[") && second.contains("]")) { // second is memory
-
-				} else if (second.contains("ax")) {
-					for (int i = 3; i >= 0; i--) {
-						dx[i] = ax[i];
-					}
-				} else if (second.contains("bx")) {
-					for (int i = 3; i >= 0; i--) {
-						dx[i] = bx[i];
-					}
-				} else if (second.contains("cx")) {
-					for (int i = 3; i >= 0; i--) {
-						dx[i] = cx[i];
-					}
-				} else if (second.contains("l") || second.contains("h")) {// error
-					System.out.println("Error: Byte/Word Combination Not Allowed");
-					System.exit(0);
-				} else {// number
-
-				}
-			} else if (first.equalsIgnoreCase("al")) {
-
-			} else if (first.equalsIgnoreCase("ah")) {
-
-			} else if (first.equalsIgnoreCase("bl")) {
-
-			} else if (first.equalsIgnoreCase("bh")) {
-
-			} else if (first.equalsIgnoreCase("cl")) {
-
-			} else if (first.equalsIgnoreCase("ch")) {
-
-			} else if (first.equalsIgnoreCase("dl")) {
-
-			} else if (first.equalsIgnoreCase("dh")) {
-
-			}
 		} else { // reg veya memoryye yazmýyo hata ver
 			System.out.println("Undefined sysbols are listed");
 			System.exit(0);
