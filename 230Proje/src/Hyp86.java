5ab81d0b4361a5c31765baa0c1653b6fe2c85cd0
Veyis Turgut <vturgut68@gmail.com>
2020-03-12 21:17:40 +0300

    Update Hyp86.java
    mov [ax|bx|cx|dx], di si bp sp tarzı şeyler ne işe yarıyo öğrenip eklenilcek

--- 230Proje/src/Hyp86.java
+++ 230Proje/src/Hyp86.java
@@ -2,22 +2,26 @@ import java.io.File;
 
 public class Hyp86 {
 
-	byte[] memory = new byte[64 * 1024];
-	// hexa tutalým
+	byte[] memory = new byte[64 * 1024]; // hexa tutalým
+
+	// bu 4 ü ne ise yarýyo bilmiyorum henuz
 	char[] di = new char[4];
 	char[] sp = new char[4];
 	char[] si = new char[4];
 	char[] bp = new char[4];
-	char[] ax = new char[4];
+
+	char[] ax = new char[4]; // ah:al seklinde tutuyoruz. ilk 2 indis ah, son 2 indis al
 	char[] bx = new char[4];
 	char[] cx = new char[4];
 	char[] dx = new char[4];
+
 	boolean ZF = false;
 	boolean AF = false;
 	boolean CF = false;
-	String SF = "FFFE";
-	int MP = 0; // memory Pointer
 	boolean OF = false;
+
+	String SF = "FFFE"; // stack pointer , memoryye erisirken hexadan decimale cevircez
+	int MP = 0; // memory Pointer
 
 	Hyp86(String s) { // constructor
 
@@ -51,7 +55,7 @@ public class Hyp86 {
 			}
 		} else if (second.equals("dx")) {
 			for (int i = 3; i >= 0; i--) {
-				ax[i] = dx[i - 4];
+				ax[i] = dx[i];
 			}
 		} else if (second.contains("l") || second.contains("h")) {// error
 			System.out.println("Error: Byte/Word Combination Not Allowed");
@@ -132,15 +136,113 @@ public class Hyp86 {
 	}
 
 	public void mov_al_unknown(String second) {
+		if (second.equals("ah")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = ax[i - 2];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = bx[i - 2];
+			}
+		} else if (second.equals("cl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = cx[i];
+			}
+		} else if (second.equals("ch")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = cx[i - 2];
+			}
+		} else if (second.equals("dl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = dx[i];
+			}
+		} else if (second.equals("dh")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i] = dx[i - 2];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
 
+		}
 	}
 
 	public void mov_ah_unknown(String second) {
+		if (second.equals("al")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = ax[i];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = bx[i - 2];
+			}
+		} else if (second.equals("cl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = cx[i];
+			}
+		} else if (second.equals("ch")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = cx[i - 2];
+			}
+		} else if (second.equals("dl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = dx[i];
+			}
+		} else if (second.equals("dh")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = dx[i - 2];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
 
+		}
 	}
 
 	public void mov_bl_unknown(String second) {
-
+		if (second.equals("al")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = ax[i];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = bx[i - 2];
+			}
+		} else if (second.equals("cl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = cx[i];
+			}
+		} else if (second.equals("ch")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = cx[i - 2];
+			}
+		} else if (second.equals("dl")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = dx[i];
+			}
+		} else if (second.equals("dh")) {
+			for (int i = 3; i >= 2; i--) {
+				ax[i - 2] = dx[i - 2];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
+		}
 	}
 
 	public void mov_bh_unknown(String second) {
@@ -148,19 +250,149 @@ public class Hyp86 {
 	}
 
 	public void mov_cl_unknown(String second) {
+		if (second.equals("al")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = ax[i];
+			}
+		} else if (second.equals("ah")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = ax[i - 2];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = bx[i - 2];
+			}
+		} else if (second.equals("ch")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = cx[i - 2];
+			}
+		} else if (second.equals("dl")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = dx[i];
+			}
+		} else if (second.equals("dh")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i] = dx[i - 2];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
 
+		}
 	}
 
 	public void mov_ch_unknown(String second) {
-
+		if (second.equals("al")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = ax[i];
+			}
+		} else if (second.equals("ah")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = ax[i - 2];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = bx[i - 2];
+			}
+		} else if (second.equals("cl")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = cx[i];
+			}
+		} else if (second.equals("dl")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = dx[i];
+			}
+		} else if (second.equals("dh")) {
+			for (int i = 3; i >= 2; i--) {
+				cx[i - 2] = dx[i - 2];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
+		}
 	}
 
 	public void mov_dl_unknown(String second) {
+		if (second.equals("al")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = ax[i];
+			}
+		} else if (second.equals("ah")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = ax[i - 2];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = bx[i - 2];
+			}
+		} else if (second.equals("cl")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = cx[i];
+			}
+		} else if (second.equals("ch")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = cx[i - 2];
+			}
+		} else if (second.equals("dh")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i] = dx[i - 2];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
 
+		}
 	}
 
 	public void mov_dh_unknown(String second) {
-
+		if (second.equals("al")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = ax[i];
+			}
+		} else if (second.equals("ah")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = ax[i - 2];
+			}
+		} else if (second.equals("bl")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = bx[i];
+			}
+		} else if (second.equals("bh")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = bx[i - 2];
+			}
+		} else if (second.equals("cl")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = cx[i];
+			}
+		} else if (second.equals("ch")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = cx[i - 2];
+			}
+		} else if (second.equals("dl")) {
+			for (int i = 3; i >= 2; i--) {
+				dx[i - 2] = dx[i];
+			}
+		} else if (second.contains("x")) {
+			System.out.println("Byte/Word Combination Not Allowed");
+			System.exit(0);
+		} else { // þu diger regler olabýlýr sonra yapýcam
+		}
 	}
 
 	public void mov_reg_unknown(String first, String second) {
@@ -201,7 +433,7 @@ public class Hyp86 {
 		} else if (first.equals("ax") || first.equals("cx") || first.equals("bx") || first.equals("dx")
 				|| first.equals("ah") || first.equals("al") || first.equals("bl") || first.equals("bh")
 				|| first.equals("ch") || first.equals("cl") || first.equals("dl") || first.equals("dh")) { // first is
-																											// reg
+			// reg
 			mov_reg_unknown(first, second);
 
 		} else { // reg veya memoryye yazmýyo hata ver
